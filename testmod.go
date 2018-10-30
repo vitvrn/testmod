75ad9a7557b13760038247413b7b6f986ced8abb
Пономарев Виталий Александрович <vitaliy.ponomarev@south.rt.ru>
2018-10-30 11:08:27 +0300

    Change Hi to allow multilang

--- testmod.go
+++ testmod.go
@@ -1,9 +1,23 @@
 // testmod.go
 package testmod
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// Hi returns a firendly greeting
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s!", name)
+// Hi returns a firendly greeting in language lang
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
